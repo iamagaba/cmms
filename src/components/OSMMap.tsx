badb8717e688024935e64b2e642b43c6fb3dc130
iamagaba <brucebyarugaba1@gmail.com>
2025-09-10 18:25:38 +0300

    [dyad] Fixed a typo in the OSMMap component. - wrote 1 file(s)

--- src/components/OSMMap.tsx
+++ src/components/OSMMap.tsx
@@ -4,7 +4,7 @@ import L from 'leaflet';
 import { theme } from 'antd';
 
 // Fix for default Leaflet icon issues with Webpack/Vite
-delete (L.Icon.Default.prototype as any)._get  IconUrl;
+delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
   iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
